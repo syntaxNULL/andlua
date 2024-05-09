95bb6187b9bdbd5534095d0743da38bbbae6e42b
syntaxNULL <103994429+syntaxNULL@users.noreply.github.com>
2024-05-09 21:41:41 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
-tesy
+require "import"
+import "android.app.*"
+import "android.os.*"
+import "android.widget.*"
+import "android.view.*"
+
+activity.setTheme(R.AndLua13)
+activity.actionBar.hide()
+activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS).setStatusBarColor(0xFF000000);
+
+Toast.makeText(activity, "kupal",Toast.LENGTH_SHORT).show()
