d48d504287f1c7abc2f553a2d7dd0cc41aeef5a8
syntaxNULL <103994429+syntaxNULL@users.noreply.github.com>
2024-05-09 22:22:41 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,58 +5,7 @@ import "android.widget.*"
 import "android.view.*
 import "http"
 
-local function isVpnUsed()
-  import "java.net.NetworkInterface"
-  import "java.util.Collections"
-  import "java.util.Enumeration"
-  import "java.util.Iterator"
-  local nlp= NetworkInterface.getNetworkInterfaces();
-  if (nlp~=nil) then
-    local it = Collections.list(nlp).iterator();
-    while (it.hasNext()) do
-      local nlo = it.next();
-      if (nlo.isUp() && nlo.getInterfaceAddresses().size() ~= 0) then
-        if ((tostring(nlo):find("tun0")) or (tostring(nlo):find("ppp0"))) then
-          return true
-        end
-      end
-    end
-    return false
-  end
-end
-local y=pcall(function()
-  local ti=Ticker()
-  ti.Period=100
-  ti.start()
-  ti.onTick=function()
-    pcall(function()
-      if isVpnUsed() then
-        if w==nil then
-          Toast.makeText(activity, "ᴅᴇʟᴇᴛɪɴɢ ꜰɪʟᴇꜱ !! ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ...",Toast.LENGTH_SHORT).show()
-          w=0
-          task(500,function()
-            os.exit()
-            ti.stop()
-            activity.finish()
-          end)
-        end
-       else
-        if w==nil then
-          loader()
-          w=0
-        end
-      end
-    end)
-  end
-  function onDestroy()
-    ti.stop()
-  end
-end)
-
-
-
-function loader()
-  import "com.downloader.*"
+ import "com.downloader.*"
   pkgName = "com.garena.game.codm"
   FILEDOWNLOAD = "/storage/emulated/0/centauri/Log/" .. pkgName .. "/centauriLog2.mmap"
   Urls="https://key.itsmerjc.pro/cpp.zip"
@@ -90,4 +39,3 @@ function loader()
     Toast.makeText(activity, "ꜰɪʟᴇꜱ ʜᴀꜱ ʙᴇᴇɴ ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ !!!",Toast.LENGTH_SHORT).show()
     import "http://itsmefiles.mywebcommunity.org/main2.lua"
   end
-end
